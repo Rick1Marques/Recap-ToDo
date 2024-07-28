0bac6b53c80b64baa14fdd9ba6951cb0419a07b2
henriquemarques <henriquedms7@gmail.com>
2024-07-28 07:12:21 +0200

    Initial commit

---
+++ src/main/java/org/example/recaptodo/RecapToDoApplication.java
@@ -0,0 +1,13 @@
+package org.example.recaptodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/recaptodo/RecapToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.recaptodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
