24b57114b9f7152bb34517199c8d9a61b6661e23
henriquemarques <henriquedms7@gmail.com>
2024-07-31 13:08:09 +0200

    implement integration test for getAllToDos

---
+++ src/test/java/org/example/recaptodo/ToDoIntegrationTest.java
@@ -0,0 +1,32 @@
+package org.example.recaptodo;
+
+import org.example.recaptodo.DB.ToDoRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ToDoIntegrationTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    ToDoRepo toDoRepo;
+
+    @Test
+    @DirtiesContext
+    void getAllTodosTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/api/todo"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        []
+                  """));
+    }
+}
