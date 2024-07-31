ed60d8c1b0f705daf8100c45111d5534715258a9
henriquemarques <henriquedms7@gmail.com>
2024-07-31 08:06:12 +0200

    implement test for findAllTodos

--- src/test/java/org/example/recaptodo/service/ToDoServiceTest.java
+++ src/test/java/org/example/recaptodo/service/ToDoServiceTest.java
@@ -7,6 +7,8 @@ import org.example.recaptodo.model.ToDoStatus;
 import org.example.recaptodo.utils.IdService;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -34,4 +36,22 @@ class ToDoServiceTest {
 
     }
 
+
+    @Test
+    void findAllTodos() {
+
+        ToDo toDoOne = new ToDo("123", "todo1", ToDoStatus.OPEN);
+        ToDo toDoTwo = new ToDo("321", "todo2", ToDoStatus.OPEN);
+        List<ToDo> toDoList = List.of(toDoOne, toDoTwo);
+        when(mockToDoRepository.findAll()).thenReturn(toDoList);
+
+        ToDoService toDoService = new ToDoService(mockToDoRepository, mockIdService);
+
+        List<ToDo> result = toDoService.findAllTodos();
+
+        verify(mockToDoRepository).findAll();
+
+        assertEquals(toDoList, result);
+
+    }
 }
