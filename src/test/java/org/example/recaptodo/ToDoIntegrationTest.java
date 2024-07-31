0bce2277037d3014498dfdf77482f2804ad0b0d7
henriquemarques <henriquedms7@gmail.com>
2024-07-31 15:06:33 +0200

    postToDo check id field

--- src/test/java/org/example/recaptodo/ToDoIntegrationTest.java
+++ src/test/java/org/example/recaptodo/ToDoIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -49,7 +49,7 @@ public class ToDoIntegrationTest {
                   "description": "testing"
                 }
                 """
-));
-
+))
+                .andExpect(jsonPath("$.id").exists());
     }
 }
